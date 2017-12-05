48589c940c163a1d0b5a44eebb06740798e4a479
VaporUser <enryruggi@hotmail.it>
2017-12-05 20:01:18 +0100

    restructured the html index composition

---
+++ implementation/web_interface/js/change_context.js
@@ -0,0 +1,20 @@
+
+// jQuery script to load the html pages on index.html
+
+$(function (){
+
+    // Default load the welcome page
+    $(document).ready(function () {
+        $("#stage").load("./html/welcome.html");
+    });
+
+    // load preference page
+    $("#preferences").click(function(){
+        $("#stage").load("./html/preferences.html");
+    });
+
+    //load sidebar menu
+    $("#menu-toggle").click(function(e){
+        $("#wrapper").toggleClass("toggled");
+    });
+});
