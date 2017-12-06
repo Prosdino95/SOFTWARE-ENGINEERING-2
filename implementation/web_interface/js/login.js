0c2c683f5e4dbe0db12b38ddb4c13c6530c90ec2
VaporUser <enryruggi@hotmail.it>
2017-12-06 14:00:08 +0100

    added javascript check on text field

--- implementation/web_interface/js/login.js
+++
@@ -1,3 +0,0 @@
-function print() {
-	alert("Login Failed");			
-}
---
+++ implementation/web_interface/js/sign_up.js
@@ -0,0 +1,52 @@
+
+// check if password and retype password are the same
+function passwordCheck(password, retype_password) {
+    return password === retype_password;
+
+}
+
+// check valid email
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+// sign up -- registration handler
+$(function() {
+    $(document).submit(function() {
+
+        var user = $('#name').val();
+        var email = $('#email').val();
+        var pass = $('#password').val();
+        var retype_pass = $('#retype-password').val();
+
+        // Check passwords match
+        if(!passwordCheck(pass, retype_pass)){
+            alert("Passwords do not match");
+            throw error();
+        };
+
+        // Check valid email
+        if(!validateEmail(email)){
+            alert("Not valid email address");
+            throw error();
+        };
+
+        // Post request
+        alert("OK");
+        $.ajax({
+            url: 'http://127.0.0.1:5000/registration',
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            type: 'post',
+            data: JSON.stringify( { "name": user, "password": pass, "email": email } ),
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+
+});
