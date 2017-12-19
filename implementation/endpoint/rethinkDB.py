482cda2ac3d4eb8095402a1ca69da848972b7fd7
VaporUser <enryruggi@hotmail.it>
2017-12-19 17:07:52 +0100

    fixed db name

--- implementation/endpoint/rethinkDB.py
+++ implementation/endpoint/rethinkDB.py
@@ -3,13 +3,13 @@ import rethinkdb as r
 
 def init_bd():
     r.connect("localhost", 28015).repl()
-    r.db_create("Travelander").run()
-    r.db("Travelander").table_create("user", primary_key='email').run()
+    r.db_create("Travlendar").run()
+    r.db("Travlendar").table_create("user", primary_key='email').run()
 
-    r.db("Travelander").table_create("event").run()
-    r.db("Travelander").table("event").index_create("eventKey", [r.row["eventName"], r.row["startingTime"]]).run()
+    r.db("Travlendar").table_create("event").run()
+    r.db("Travlendar").table("event").index_create("eventKey", [r.row["eventName"], r.row["startingTime"]]).run()
 
-    r.db("Travelander").table_create("event_submit", primary_key='event').run()
+    r.db("Travlendar").table_create("event_submit", primary_key='event').run()
 
 
 if __name__ == "__main__":
