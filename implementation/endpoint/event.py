928f02db05e3da1a008bd437f0f1445f9163d052
Marco Prosdocimi <prosdino95@gmail.com>
2017-12-19 11:45:00 +0100

    add check_overlays

--- implementation/endpoint/event.py
+++ implementation/endpoint/event.py
@@ -1,6 +1,5 @@
 import rethinkdb as r
 from tokenDB import token_query
-import json
 
 
 def add_event(event):
@@ -11,13 +10,14 @@ def add_event(event):
     cursor = r.table("event").insert(event).run()
     id = cursor["generated_keys"][0]
     r.table("event_submit").insert({"event": id, "email": email}).run()
-    # check_overlays(token)
+    check_overlays(token)
 
 
 def del_event(token, event_id):
     r.connect("localhost", 28015, "Travelander").repl()
     r.table("event").get(event_id).delete().run()
-    # check_overlays(token)
+    r.table("event_submit").get(event_id).delete().run()
+    check_overlays(token)
 
 
 def mod_event(event):
@@ -25,12 +25,20 @@ def mod_event(event):
     token = event["token"]
     del event["token"]
     r.table("event").get(event["id"]).update(event).run()
-    # check_overlays(token)
-
+    check_overlays(token)
 
 
 def check_overlays(token):
-    # TODO work in progress
+    events_list = get_event(token)
+    for i in events_list:
+        alarm = False
+        for j in events_list:
+            if i != j and j["starting_time"] < i["finish_tme"]:
+                if j["finish_tme"] > i["starting_time"]:
+                    set_alarm(i, j)
+                    alarm = True
+        if not alarm:
+            r.table("event").get(i["id"]).update({"alarm": False}).run()
     return "all ok"
 
 
@@ -48,7 +56,7 @@ def get_event(token):
         eq_join("event", r.table("event")). \
         without({"left": ["email", "event"]}).\
         zip().run()
-    return e
+    return list(e)
 
 
 def get_email(token):
