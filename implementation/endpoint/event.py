767f03ee10f548062fc4d6f332e80dc0af72dd9e
Marco Prosdocimi <prosdino95@gmail.com>
2017-12-21 17:42:40 +0100

    flexible lunch

--- implementation/endpoint/event.py
+++ implementation/endpoint/event.py
@@ -1,16 +1,16 @@
 import rethinkdb as r
 from tokenDB import token_query
-from datetime import datetime
-from calendar import timegm
+from flexible_lunch import rearrange_lunch
 
 
 def add_event(event):
     r.connect("localhost", 28015, "Travlendar").repl()
     token = event["token"]
     del event["token"]
+    event["flexible_lunch"] = False
+    event["start"] += "+00:00"
+    event["end"] += "+00:00"
     email = get_email(token)
-    event["start"] = iso8601_to_epoch(event["start"])
-    event["end"] = iso8601_to_epoch(event["end"])
     cursor = r.table("event").insert(event).run()
     id = cursor["generated_keys"][0]
     r.table("event_submit").insert({"event": id, "email": email}).run()
@@ -27,9 +27,6 @@ def del_event(token, event_id):
 def mod_event(event):
     r.connect("localhost", 28015, "Travlendar").repl()
     token = event["token"]
-    del event["token"]
-    event["start"] = iso8601_to_epoch(event["start"])
-    event["end"] = iso8601_to_epoch(event["end"])
     r.table("event").get(event["id"]).update(event).run()
     check_overlays(token)
 
@@ -41,8 +38,11 @@ def check_overlays(token):
         for j in events_list:
             if i != j and j["start"] < i["end"]:
                 if j["end"] > i["start"]:
-                    set_alarm(i, j)
                     alarm = True
+                    if i["flexible_lunch"]:
+                        rearrange_lunch(i, j)
+                    else:
+                        set_alarm(i, j)
         if not alarm:
             r.table("event").get(i["id"]).update({"alarm": False}).run()
     return "all ok"
@@ -58,27 +58,14 @@ def set_alarm(e1, e2):
 def get_event(token):
     r.connect("localhost", 28015, "Travlendar").repl()
     email = get_email(token)
-    e = r.table("event_submit").filter(r.row["email"] == email).\
+    query = r.table("event_submit").filter(r.row["email"] == email).\
         eq_join("event", r.table("event")). \
         without({"left": ["email", "event"]}).\
         zip().run()
-    return list_iso8601_to_epoch(list(e))
+    events = list(query)
+    return events
 
 
 def get_email(token):
     return token_query(token)
 
-
-def epoch_to_iso8601(timestamp):
-    return datetime.fromtimestamp(timestamp).isoformat()
-
-
-def list_iso8601_to_epoch(json_list):
-    for e in json_list:
-        e["start"] = epoch_to_iso8601(e["start"])
-        e["end"] = epoch_to_iso8601(e["end"])
-    return json_list
-
-
-def iso8601_to_epoch(datestring):
-    return timegm(datetime.strptime(datestring, "%Y-%m-%d %H:%M").timetuple())
---
+++ implementation/endpoint/flexible_lunch.py
@@ -0,0 +1,61 @@
+import rethinkdb as r
+from tokenDB import token_query
+from datetime import time, datetime, timedelta
+
+r.connect("localhost", 28015, "Travlendar").repl()
+
+
+def set_lunch(option):
+    email = token_query(option["token"])
+    del option["token"]
+    r.table("user").get(email).update({"lunch": option}).run()
+    create_lunch(email)
+
+
+def create_lunch(email):
+    lunch = r.table("user").get(email).get_field("lunch").run()
+    start_hour = time(lunch["launch_start"])
+    today = datetime.today().replace(hour=start_hour.hour, minute=start_hour.minute)
+    end_day = today + timedelta(days=30)
+    for timestamp in datespan(today, end_day):
+        start = timestamp
+        end = start + timedelta(minutes=30)
+        create_lunch_event(start, end, email)
+
+
+def create_lunch_event(start, end, email):
+    lunch_event = {"alarm": False,
+                   "color": "#FF5252",
+                   "editable": False,
+                   "title": "lunch",
+                   "flexible_lunch": True,
+                   "start": start.strftime("%Y-%m-%d %H:%M+00:00"),
+                   "end": end.strftime("%Y-%m-%d %H:%M+00:00"),
+                   }
+    cursor = r.table("event").insert(lunch_event).run()
+    id = cursor["generated_keys"][0]
+    r.table("event_submit").insert({"event": id, "email": email}).run()
+
+
+def rearrange_lunch(lunch, e):
+    end_lunch_time = r.table("user").get("bbb@io.com").get_field("lunch").run()
+    end_lunch_time = time(hour=datetime.strptime(end_lunch_time["lunch_end"], "%H:%M").hour,
+                          minute=datetime.strptime(end_lunch_time["lunch_end"], "%H:%M").minute)
+    new_end = datetime.strptime(e["end"], "%Y-%m-%d %H:%M+00:00") + timedelta(minutes=30)
+    new_end = new_end.strftime("%Y-%m-%d %H:%M+00:00")
+    lunch_end = time(hour=datetime.strptime(new_end, "%Y-%m-%d %H:%M+00:00").hour,
+                     minute=datetime.strptime(new_end, "%Y-%m-%d %H:%M+00:00").minute)
+    if lunch_end > end_lunch_time:
+        lunch["alarm"] = True
+        r.table("event").get(lunch["id"]).update(lunch).run()
+    else:
+        lunch["start"] = e["end"]
+        lunch["end"] = new_end
+        r.table("event").get(lunch["id"]).update(lunch).run()
+
+
+def datespan(start_date, end_date, delta=timedelta(days=1)):
+    current_date = start_date
+    while current_date < end_date:
+        yield current_date
+        current_date += delta
