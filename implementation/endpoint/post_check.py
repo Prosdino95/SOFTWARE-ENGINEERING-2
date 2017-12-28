e2851213b30598d8866c0e83bb5856ea8f350ab8
Marco Prosdocimi <prosdino95@gmail.com>
2017-12-28 23:52:42 +0100

    security upgrade v1.0

---
+++ implementation/endpoint/post_check.py
@@ -0,0 +1,58 @@
+from jsonschema import validate
+from json import load
+
+
+def registration(json):
+    schema = load(open("resource/jsonSchemes/registration.json"))
+    validate(json, schema)
+
+
+def login(json):
+    schema = load(open("resource/jsonSchemes/login.json"))
+    validate(json, schema)
+
+
+def mod_password(json):
+    schema = load(open("resource/jsonSchemes/mod_password_profile.json"))
+    validate(json, schema)
+
+
+def preference(json):
+    schema = load(open("resource/jsonSchemes/preference.json"))
+    validate(json, schema)
+
+
+def user_profile(json):
+    schema = load(open("resource/jsonSchemes/user_profile.json"))
+    validate(json, schema)
+
+
+def add_event(json):
+    schema = load(open("resource/jsonSchemes/add_event.json"))
+    validate(json, schema)
+
+
+def mod_event(json):
+    schema = load(open("resource/jsonSchemes/mod_event.json"))
+    validate(json, schema)
+
+
+def del_event(json):
+    schema = load(open("resource/jsonSchemes/del_event.json"))
+    validate(json, schema)
+
+
+def get_route(json):
+    schema = load(open("resource/jsonSchemes/route.json"))
+    validate(json, schema)
+
+
+def flexible_lunch(json):
+    schema = load(open("resource/jsonSchemes/flexible_lunch.json"))
+    validate(json, schema)
+
+
+
+if __name__ == "__main__":
+    schema = load(open("User_test.json"))
+    registration(schema)
