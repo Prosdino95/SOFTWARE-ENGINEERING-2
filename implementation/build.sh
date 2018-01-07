9bce7ad6b63d8757603f49d5d39489189a7023ac
vercesig <giacomo.vercesi@mail.polimi.it>
2018-01-07 16:03:16 +0100

    Build script fix

--- implementation/build.sh
+++ implementation/build.sh
@@ -1,7 +1,7 @@
 docker build -t travlendar_endpoint -f endpoint.Dockerfile .
 docker build -t travlendar_optimalroute -f optimalroute.Dockerfile .
 docker build -t travlendar_scraper -f scraper.Dockerfile .
-pushd valhalla_server
-docker build -t valhalla_server_milan .
-popd
 docker build -t travlendar_webinterface -f web_interface.Dockerfile .
+cd valhalla_server
+docker build -t valhalla_server_milan .
+cd ..
